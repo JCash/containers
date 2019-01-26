157f483562149e3ea62e7425f87c4dff097bcb82
JCash <mwesterdahl76@gmail.com>
2019-01-26 18:20:24 +0100

    fix

--- compile_cl.bat
+++ compile_cl.bat
@@ -1,8 +1,8 @@
-echo off
+echo on
 
 if NOT DEFINED VCINSTALLDIR (
     if exist "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" (
-        call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64
+        call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
         echo "USING VISUAL STUDIO 17"
     )
 )
@@ -34,6 +34,8 @@ if NOT DEFINED VCINSTALLDIR (
         echo "USING VISUAL STUDIO 12"
     )
 )
+
+echo "Using " %VCINSTALLDIR%
 
 if NOT DEFINED VCINSTALLDIR (
     echo "No compatible visual studio found! run vcvarsall.bat first!"
